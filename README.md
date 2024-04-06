5b4adce9d3453cf8564f8dc14d6d52bea1ddcfa7
Rashmi Patil <hiitsrash24@gmail.com>
2024-04-06 14:14:22 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# rock-paper-scissors
+ This repository showcases a playful recreation of the game, Rock, Paper and Scissors, using HTML, CSS and JavaScript.
---
+++ script.js
@@ -0,0 +1,63 @@
+let userScore = 0;
+let compScore = 0;
+
+const choices = document.querySelectorAll(".choice");
+const msg = document.querySelector("#msg");
+
+const userScorePara = document.querySelector("#user-score");
+const compScorePara = document.querySelector("#comp-score");
+
+const genCompChoice = () => {
+  const options = ["rock", "paper", "scissors"];
+  const randChoice = Math.floor(Math.random() * 3); //3 since we have to generate no's b/w 0 to 2 as indices are frm 0 to 2.
+  return options[randChoice];
+};
+
+const drawGame = () => {
+  msg.innerText = "It's a Draw";
+  msg.style.backgroundColor = "#76ABAE";
+};
+
+const showWinner = (userWin, userChoice, compChoice) => {
+  if (userWin) {
+    userScore++;
+    userScorePara.innerText = userScore;
+    msg.innerText = ` Congratulations, You won! ${userChoice} beats ${compChoice}`;
+    msg.style.backgroundColor = "green";
+  } else {
+    compScore++;
+    compScorePara.innerText = compScore;
+    msg.innerText = `Oops! Try Again. ${compChoice} beats ${userChoice}`;
+    msg.style.backgroundColor = "red";
+  }
+};
+
+const playGame = (userChoice) => {
+  //Generate computer choice
+  const compChoice = genCompChoice();
+
+  if (userChoice === compChoice) {
+    //Draw Game
+    drawGame();
+  } else {
+    let userWin = true;
+    if (userChoice === "rock") {
+      //scissors, paper
+      userWin = compChoice === "paper" ? false : true;
+    } else if (userChoice === "paper") {
+      //rock, scissors
+      userWin = compChoice === "scissors" ? false : true;
+    } else {
+      //rock, paper
+      userWin = compChoice === "rock" ? false : true;
+    }
+    showWinner(userWin, userChoice, compChoice);
+  }
+};
+
+choices.forEach((choice) => {
+  choice.addEventListener("click", () => {
+    const userChoice = choice.getAttribute("id");
+    playGame(userChoice);
+  });
+});
